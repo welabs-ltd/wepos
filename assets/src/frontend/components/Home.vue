9f127852bded8eb3dbf3e6c6ebbdbc56a153c065
haideralibd <haider4uiu@gmail.com>
2025-03-24 09:54:00 +0600

    remove Process Payment btn from pos

--- assets/src/frontend/components/Home.vue
+++ assets/src/frontend/components/Home.vue
@@ -593,7 +593,7 @@
 
                         <div class="footer wepos-clearfix">
                             <a href="#" class="back-btn wepos-left" @click.prevent="backToSale()">{{ __( 'Back to Sale', 'wepos' ) }}</a>
-                            <button class="process-checkout-btn wepos-right" @click.prevent="processPayment" :disabled="! $store.getters['Order/getCanProcessPayment']">{{ __( 'Process Payment', 'wepos' ) }}</button>
+                            <!-- <button class="process-checkout-btn wepos-right" @click.prevent="processPayment" :disabled="! $store.getters['Order/getCanProcessPayment']">{{ __( 'Process Payment', 'wepos' ) }}</button> -->
                         </div>
                     </div>
                 </div>
